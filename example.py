e4ebafe459df302e40652238f0d35098a46d42d9
voss3949 <54409097+voss3949@users.noreply.github.com>
2021-09-03 22:30:39 -0400

    Create example.py

--- example.py
+++ example.py
@@ -1,20 +1,14 @@
-while True:
+While True:
     import random
-    
+    import time
     choices=[["rock","scissor"],["paper","rock"],["scissor","paper"]]
     choices
     computer = random.choice(choices)
     computer
     self = input('rock, paper, scissors?  ')
-    self
     
     'Loser' if self in computer[1] else 'Winner' if self not in computer else 'Tie'
+    time.sleep(5)
     continue
 
-#str('You chose '+self[0])+' '+'Computer chose '+str(computer[0]) if self == computer else "Winner. "+str(self[0])+" beats " + str(computer[1]) if self[1] in computer[0] else 'Loser. ' +str(self[1]+' beats '+computer[0])
 
-#if computer == self:
-#    print("You and computer tied. Computer chose "+str(computer[1])+" and you chose "+str(self[1])
-#elif self[0] in computer[1]:
-#    print("You Win. Computer chose "+str(computer[1]+" and you chose "+str("computer[0])
-#else ''
