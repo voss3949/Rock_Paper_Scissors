15830eb3b16ad57e7d7bede7b650044f76a9fb03
voss3949 <54409097+voss3949@users.noreply.github.com>
2021-09-03 22:08:41 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,19 @@
 import random
 
 choices=[["rock","scissor"],["paper","rock"],["scissor","paper"]]
+choices
 computer = random.choice(choices)
-self = random.choice(choices)
+computer
+self = input('rock, paper, scissors?  ')
 
-str('You chose '+self[0])+' '+'Computer chose '+str(computer[0]) if self == computer else "Winner" if self[1] in computer[0] else 'Loser'
+
+'Loser' if self in computer[1] else 'Winner' if self not in computer else 'Loser'
+
+
+#str('You chose '+self[0])+' '+'Computer chose '+str(computer[0]) if self == computer else "Winner. "+str(self[0])+" beats " + str(computer[1]) if self[1] in computer[0] else 'Loser. ' +str(self[1]+' beats '+computer[0])
+
+#if computer == self:
+#    print("You and computer tied. Computer chose "+str(computer[1])+" and you chose "+str(self[1])
+#elif self[0] in computer[1]:
+#    print("You Win. Computer chose "+str(computer[1]+" and you chose "+str("computer[0])
+#else ''
