0b0ca1fd00adb496f8a25a613f4ba04796da8e85
voss3949 <54409097+voss3949@users.noreply.github.com>
2021-09-03 22:59:09 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
-While True:
+while True:
     import random
     import time
-    choices=[["rock","scissor"],["paper","rock"],["scissor","paper"]]
+    choices=[["rock","scissors"],["paper","rock"],["scissors","paper"]]
     choices
     computer = random.choice(choices)
-    computer
     self = input('rock, paper, scissors?  ')
-    
+    self
+    computer
     'Loser' if self in computer[1] else 'Winner' if self not in computer else 'Tie'
     time.sleep(5)
+    if input('Quit? Yes or No.') == 'Yes': break
     continue
 
 
