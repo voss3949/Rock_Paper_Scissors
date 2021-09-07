ff6e14215083ba894238c08c97547d8c8cb71c10
voss3949 <54409097+voss3949@users.noreply.github.com>
2021-09-06 21:29:09 -0400

    Update example.py

--- example.py
+++ example.py
@@ -7,11 +7,11 @@ while True:
     computer = random.choice(choices)
     print("rock, paper, scissors... GO!")
     player = input('rock, paper, scissors?  ')
-    time.sleep(5)
-    computer
+    time.sleep(3)
+    computer[0]
     player
     'Loser' if player in computer[1] else 'Winner' if player not in computer else 'Tie'
-    time.sleep(5)
+    time.sleep(3)
     if input('Quit? Yes or No. ') == 'Yes': break
     continue
 
