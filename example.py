6fb1fc85072c310c23154f2809e07d6b6aa38041
voss3949 <54409097+voss3949@users.noreply.github.com>
2021-09-08 16:04:41 -0400

    Update example.py

--- example.py
+++ example.py
@@ -4,8 +4,7 @@ import time
 while True:
     choices=[["rock","scissors"],["paper","rock"],["scissors","paper"]]
     computer = random.choice(choices)
-    print("rock, paper, scissors... GO!")
-    player = input('rock, paper, scissors?  ')
+    player = input("rock, paper, scissors... GO!")
     time.sleep(3)
     'Loser' if player in computer[1] else 'Winner' if player not in computer else 'Tie'
     time.sleep(3)
